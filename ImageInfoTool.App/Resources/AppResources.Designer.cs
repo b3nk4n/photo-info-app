c6446d094b1e02f010d2c9cd0180c986d0d3d540
bsautermeister <apps@bsautermeister.de>
2014-06-29 20:49:40 +0200

    Version 1.4.1 with Translations for Indonesian and Spanish.

--- ImageInfoTool.App/Resources/AppResources.Designer.cs
+++ ImageInfoTool.App/Resources/AppResources.Designer.cs
@@ -124,7 +124,7 @@ namespace ImageInfoTool.App.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die 1.4 ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die 1.4.1 ähnelt.
         /// </summary>
         public static string ApplicationVersion {
             get {

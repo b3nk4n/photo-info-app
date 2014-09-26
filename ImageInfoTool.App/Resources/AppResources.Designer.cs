09a0d9d816cfcd86798fcd16d54c5f1e3df60352
bsautermeister <apps@bsautermeister.de>
2014-09-26 16:50:45 +0200

    Added polish translation.

--- ImageInfoTool.App/Resources/AppResources.Designer.cs
+++ ImageInfoTool.App/Resources/AppResources.Designer.cs
@@ -124,7 +124,7 @@ namespace ImageInfoTool.App.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die 1.4.3 ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die 1.4.4 ähnelt.
         /// </summary>
         public static string ApplicationVersion {
             get {
